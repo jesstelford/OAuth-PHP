00c6bc7af8ed66559f6facd8fd8cea6b642c3dc6
philfreo <philfreo@0abfa552-1c45-0410-b09b-a165f8c081d9>
2010-05-12 21:13:24 +0000

    removing php short tags on services.xrds.php

--- example/server/www/services.xrds.php
+++ example/server/www/services.xrds.php
@@ -52,12 +52,12 @@ echo '<?xml version="1.0" encoding="utf-8"?>' . "\n";
 	    <Type>http://oauth.net/core/1.0/parameters/uri-query</Type>
 	    <Type>http://oauth.net/core/1.0/signature/HMAC-SHA1</Type>
 	    <Type>http://oauth.net/core/1.0/signature/PLAINTEXT</Type>
-	    <URI>http://<?=$server?>/oauth/request_token</URI>
+	    <URI>http://<?php echo $server; ?>/oauth/request_token</URI>
 	</Service>
 	<Service>
 	    <Type>http://oauth.net/core/1.0/endpoint/authorize</Type>
 	    <Type>http://oauth.net/core/1.0/parameters/uri-query</Type>
-	    <URI>http://<?=$server?>/oauth/authorize</URI>
+	    <URI>http://<?php echo $server; ?>/oauth/authorize</URI>
 	</Service>
 	<Service>
 	    <Type>http://oauth.net/core/1.0/endpoint/access</Type>
@@ -65,7 +65,7 @@ echo '<?xml version="1.0" encoding="utf-8"?>' . "\n";
 	    <Type>http://oauth.net/core/1.0/parameters/uri-query</Type>
 	    <Type>http://oauth.net/core/1.0/signature/HMAC-SHA1</Type>
 	    <Type>http://oauth.net/core/1.0/signature/PLAINTEXT</Type>
-	    <URI>http://<?=$server?>/oauth/access_token</URI>
+	    <URI>http://<?php echo $server; ?>/oauth/access_token</URI>
 	</Service>
     </XRD>
 </XRDS>
